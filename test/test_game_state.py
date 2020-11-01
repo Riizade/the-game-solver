31931643eb687120d00c566860702aedb3e43b60
Adam Johnson <riizade@gmail.com>
2020-10-31 22:09:57 -0700

    fix tests

--- test/test_game_state.py
+++ test/test_game_state.py
@@ -1,5 +1,11 @@
 
-from game_state import initial_deck, initial_piles, take_action, take_turn, CardPile, GameState, PlayerAction, PlayerTurn
+from typing import List
+
+from game_state import initial_deck, initial_piles, take_action, take_turn, Card, CardPile, GameState, PlayerAction, PlayerTurn
+
+
+def card_list(ints: List[int]) -> List[Card]:
+    return [Card(i) for i in ints]
 
 
 def test_seeded_deck_randomizes_order():
@@ -16,15 +22,15 @@ def test_seeded_deck_generates_equivalent_results_from_same_seed():
 def test_generated_deck_contains_all_cards():
     for seed in range(0, 10):
         deck = initial_deck(seed)
-        for card in range(2, 100):
-            assert(card in deck)
+        for num in range(2, 100):
+            assert(Card(num) in deck)
 
 
 def test_initial_piles():
     piles = initial_piles()
     assert(len(piles) == 4)
-    ascending = CardPile(cards=[1], ascending=True)
-    descending = CardPile(cards=[100], ascending=False)
+    ascending = CardPile(cards=card_list([1]), ascending=True)
+    descending = CardPile(cards=card_list([100]), ascending=False)
     assert(piles[0] == ascending)
     assert(piles[1] == ascending)
     assert(piles[2] == descending)
@@ -32,26 +38,26 @@ def test_initial_piles():
 
 
 def test_take_turn():
-    initial_state = GameState(deck=[10, 11, 12, 13, 14, 15, 16, 17, 18], hand=[2, 3, 4, 5, 6, 7, 89, 99], piles=initial_piles())
-    initial_state_copy = GameState(deck=[10, 11, 12, 13, 14, 15, 16, 17, 18], hand=[2, 3, 4, 5, 6, 7, 89, 99], piles=initial_piles())
+    initial_state = GameState(deck=card_list([10, 11, 12, 13, 14, 15, 16, 17, 18]), hand=card_list([2, 3, 4, 5, 6, 7, 89, 99]), piles=initial_piles())
+    initial_state_copy = GameState(deck=card_list([10, 11, 12, 13, 14, 15, 16, 17, 18]), hand=card_list([2, 3, 4, 5, 6, 7, 89, 99]), piles=initial_piles())
 
     actions = [
-        PlayerAction(2, 0),
-        PlayerAction(3, 1),
-        PlayerAction(6, 1),
-        PlayerAction(99, 2),
-        PlayerAction(89, 2),
+        PlayerAction(Card(2), 0),
+        PlayerAction(Card(3), 1),
+        PlayerAction(Card(6), 1),
+        PlayerAction(Card(99), 2),
+        PlayerAction(Card(89), 2),
     ]
 
     turn = PlayerTurn(actions=actions)
 
     expected_piles = [
-        CardPile(cards=[1, 2], ascending=True),
-        CardPile(cards=[1, 3, 6], ascending=True),
-        CardPile(cards=[100, 99, 89], ascending=False),
-        CardPile(cards=[100], ascending=False),
+        CardPile(cards=card_list([1, 2]), ascending=True),
+        CardPile(cards=card_list([1, 3, 6]), ascending=True),
+        CardPile(cards=card_list([100, 99, 89]), ascending=False),
+        CardPile(cards=card_list([100]), ascending=False),
     ]
-    expected_result = GameState(deck=[10, 11, 12, 13], hand=[4, 5, 7, 14, 15, 16, 17, 18], piles=expected_piles)
+    expected_result = GameState(deck=card_list([10, 11, 12, 13]), hand=card_list([4, 5, 7, 14, 15, 16, 17, 18]), piles=expected_piles)
 
     actual_result = take_turn(initial_state, turn)
 
@@ -63,19 +69,19 @@ def test_take_turn():
 
 
 def test_take_action():
-    initial_state = GameState(deck=[4, 5, 6], hand=[2, 3], piles=initial_piles())
-    initial_state_copy = GameState(deck=[4, 5, 6], hand=[2, 3], piles=initial_piles())
+    initial_state = GameState(deck=card_list([4, 5, 6]), hand=card_list([2, 3]), piles=initial_piles())
+    initial_state_copy = GameState(deck=card_list([4, 5, 6]), hand=card_list([2, 3]), piles=initial_piles())
 
-    action = PlayerAction(2, 0)
+    action = PlayerAction(Card(2), 0)
 
     expected_piles = [
-        CardPile(cards=[1, 2], ascending=True),
-        CardPile(cards=[1], ascending=True),
-        CardPile(cards=[100], ascending=False),
-        CardPile(cards=[100], ascending=False),
+        CardPile(cards=card_list([1, 2]), ascending=True),
+        CardPile(cards=card_list([1]), ascending=True),
+        CardPile(cards=card_list([100]), ascending=False),
+        CardPile(cards=card_list([100]), ascending=False),
     ]
 
-    expected_result = GameState(deck=[4, 5, 6], hand=[3], piles=expected_piles)
+    expected_result = GameState(deck=card_list([4, 5, 6]), hand=card_list([3]), piles=expected_piles)
 
     actual_result = take_action(initial_state, action)
 
