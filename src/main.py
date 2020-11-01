0e9e6a777124c8eea210255375a54e7bf0195e34
Adam Johnson <riizade@gmail.com>
2020-10-31 21:52:37 -0700

    added random strategy

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from game_state import simulate, PrintLevel
-from strategies import greedy
+from strategies import greedy, random
 
 
 def main():
+    print("evaluating greedy strategy")
     simulate(greedy, 1000, PrintLevel.AGGREGATE)
+
+    print("evaluating random strategy")
+    simulate(random, 1000, PrintLevel.AGGREGATE)
 
 
 if __name__ == "__main__":
--- src/strategies.py
+++ src/strategies.py
@@ -1,6 +1,19 @@
-from game_state import PlayerTurn, GameState
+from random import shuffle
+from game_state import PlayerTurn, GameState, take_action
 
 
 def greedy(state: GameState) -> PlayerTurn:
-
     return state.greediest_turn
+
+
+def random(state: GameState) -> PlayerTurn:
+    valid_actions = state.all_valid_actions.copy()
+    shuffle(valid_actions)
+
+    for action in valid_actions:
+        new_state = take_action(action, state)
+        if new_state.has_one_valid_action:
+            new_valid_actions = new_state.all_valid_actions.copy()
+            shuffle(new_valid_actions)
+            second_action = new_valid_actions[0]
+            return PlayerTurn([action, second_action])
